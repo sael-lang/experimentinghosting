4485c66ee2034a2cedabc0a247071af4a0d45b43
sael-lang <hasnain6613@gmail.com>
2023-03-12 07:34:46 +0500

    adding as

--- backend/settings.py
+++ backend/settings.py
@@ -22,9 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['0.0.0.1']
+ALLOWED_HOSTS = ['web-production-5741.up.railway.app', '127.0.0.1']
 
 
+CSRF_TRUSTED_ORIGINS = ['https://web-production-5741.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
